a5743b7c5df6cedb46d4e88f124c0890a8d9a359
odilo <odilorugamba@gmail.com>
2020-09-04 11:35:04 +0200

    fixed public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-	publicPath: './public',
+	publicPath: process.env.NODE_ENV == 'production' ? './public' : '/',
 	outputDir: '../backend/public',
 	configureWebpack: {
 		resolve: {
